b41bc9543f931350a20483c32478db2b5433442a
trick77 <jan@trick77.com>
2023-12-30 12:41:01 +0100

    Add script to generate htpasswd

---
+++ generate-htpasswd.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# This script generates a htpasswd entry with proper escaping for Traefik's basic
+# authentication mechanism, making it suitable for use in a Docker Compose label.
+
+usage() {
+  echo "Usage: $0 <username> <password>"
+  exit 1
+}
+
+if [ "$#" -ne 2 ]; then
+  echo "Error: Invalid number of parameters."
+  usage
+fi
+
+if ! command -v htpasswd > /dev/null; then
+  echo "Error: htpasswd command not found. Please install it before using this script."
+  exit 1
+fi
+
+username="$1"
+password="$2"
+
+echo $(htpasswd -nbB "$username" "$password") | sed -e s/\\$/\\$\\$/g
